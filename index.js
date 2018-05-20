cde4351a8f8272d1028a85506a14fd2b342f9b0e
danykyroz <danykyroz@gmail.com>
2018-05-20 12:16:11 -0500

    App name

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const express= require('express');
 const morgan= require('morgan');
 const app=express();
 
+//Set variables
+app.set("appName","Primer Server Express");
+//Midlewares
 app.use(morgan("dev"));
 
+//Routes
 app.get('/',(req,res)=>{
   res.end("Hola mundo");
 })
 app.get('/login',(req,res)=>{
   res.end("Aca va la página de login");
 })
+
+//Todas las rutas
 app.get('*',(req,res)=>{
   res.end("Url no encontrada.");
 })
-app.listen(3000);
+
+//Iniciar servidor
+app.listen(3000,()=>{
+  console.log("Servidor escuchando 3000");
+  console.log("Nombre App",app.get("appName"));
+
+
+});
