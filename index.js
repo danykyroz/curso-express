25d1dc55dad0e308b096aa2f45785c241ab0bc6a
danykyroz <danykyroz@gmail.com>
2018-05-20 11:51:43 -0500

    Probando rutas

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ app.get('/',(req,res)=>{
 app.get('/login',(req,res)=>{
   res.end("Aca va la página de login");
 })
-
 app.get('*',(req,res)=>{
-  res.end("Url no encontrada");
+  res.end("Url no encontrada.");
 })
 app.listen(3000);
