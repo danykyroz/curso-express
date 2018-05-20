ff94043362911efc284911b5ece83abc75bf8c98
danykyroz <danykyroz@gmail.com>
2018-05-20 11:50:22 -0500

    Prando rutas

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const app=express();
 app.get('/',(req,res)=>{
   res.end("Hola mundo");
 })
+app.get('/login',(req,res)=>{
+  res.end("Aca va la página de login");
+})
+
+app.get('*',(req,res)=>{
+  res.end("Url no encontrada");
+})
 app.listen(3000);
